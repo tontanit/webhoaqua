8e9df2ec4e004793f19be5613153d5ed44e8c78e
unknown <tontanit@gmail.com>
2021-12-18 11:35:44 +0700

    add file tai nha

--- frontend/index.php
+++ frontend/index.php
@@ -36,13 +36,22 @@ require_once('../database/dbhelper.php');
                 </thead>
                 <tbody>
 
+                    <!-- Code phan trang -->
                     <?php
-                    $sql = "SELECT * FROM category";
+                    $pages = getGet('pages');
+
+                    if ($pages <= 0) {
+                        $pages = 1;
+                    }
+                    $sotin1trang = 3;
+                    $from = ($pages - 1) * $sotin1trang;
+                    $sql = "SELECT * FROM category
+                    LIMIT $from, $sotin1trang";
                     $result = executeSelect($sql);
-                    $index = 1;
+
                     foreach ($result as $list) {
                         echo '  <tr>
-                        <td>' . $index++ . '</td>
+                        <td>' . ++$from . '</td>
                        
                         <td><a href="cat_product.php?id=' . $list['id'] . '"> ' . $list['name'] . '</a></td>
                     </tr>';
@@ -51,6 +60,60 @@ require_once('../database/dbhelper.php');
 
                 </tbody>
             </table>
+            <?php
+            // Dem tong so recode trong bang category  
+            $sql = "SELECT * FROM category";
+            $row = executeSelect($sql);
+            $num_row = count($row);
+            $sum_pages = ceil($num_row / $sotin1trang);
+
+            ?>
+            <!-- Neu chi co 1 trang thi khong hien thi "phan trang" -->
+            <?php
+            if ($sum_pages > 1) {
+            ?>
+                <ul class="pagination">
+                    <!-- Khong hien Previous -->
+                    <?php
+                    if ($pages > 1) {
+                        echo '<li class="page-item"><a class="page-link" href="index.php?pages=' . ($pages - 1) . '">Previous</a></li>';
+                    }
+                    ?>
+
+
+                    <?php
+                    $avaiblePages = [1, $pages - 1, $pages + 1, $sum_pages];
+                    $isFirst = $isLast = false;
+                    for ($i = 1; $i <= $sum_pages; $i++) {
+                        //Hien thi dau ... Neu co nhieu trang
+
+                        if (!in_array($i, $avaiblePages)) {
+                            if (!$isFirst && $pages > 3) {
+                                echo '<li class="page-item"><a class="page-link" href="index.php?pages=' . ($pages - 2) . '">...</a></li>';
+                                $isFirst = true;
+                            }
+                            if (!$isLast && $i > $pages) {
+                                echo '<li class="page-item"><a class="page-link" href="index.php?pages=' . ($pages + 2) . '">...</a></li>';
+                                $isLast = true;
+                            }
+                            continue;
+                        }
+
+                        if ($pages == $i) {
+                            echo '<li class="page-item active"><a class="page-link" href="index.php?pages=' . $i . '">' . $i . '</a></li>';
+                        } else echo '<li class="page-item"><a class="page-link" href="index.php?pages=' . $i . '">' . $i . '</a></li>';
+                    }
+                    ?>
+
+                    <!-- Khong hien Next -->
+                    <?php
+                    if ($pages < $sum_pages) {
+                        echo '<li class="page-item"><a class="page-link" href="index.php?pages=' . ($pages + 1) . '">Previous</a></li>';
+                    }
+                    ?>
+                </ul>
+            <?php } ?>
+            <!-- End phan trang -->
         </div>
         </div>
 
